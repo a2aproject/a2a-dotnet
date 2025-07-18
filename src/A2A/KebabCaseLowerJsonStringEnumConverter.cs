0184d9c3f03a7a4fbc59955d5dc5862cf49236f8
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2025-07-18 09:35:03 -0700

    Back to kebab case
    compliance check tool is at odds w/ spec

--- src/A2A/KebabCaseLowerJsonStringEnumConverter.cs
+++ src/A2A/KebabCaseLowerJsonStringEnumConverter.cs
@@ -12,12 +12,3 @@ namespace A2A;
 public sealed class KebabCaseLowerJsonStringEnumConverter<TEnum>() :
     JsonStringEnumConverter<TEnum>(JsonNamingPolicy.KebabCaseLower)
     where TEnum : struct, Enum;
-
-/// <summary>
-/// A JSON string enum converter that converts enum values to snake_case lower strings.
-/// </summary>
-/// <typeparam name="TEnum">The type of the enum to convert.</typeparam>
-[EditorBrowsable(EditorBrowsableState.Never)]
-public sealed class SnakeCaseLowerJsonStringEnumConverter<TEnum>() :
-    JsonStringEnumConverter<TEnum>(JsonNamingPolicy.SnakeCaseLower)
-    where TEnum : struct, Enum;
