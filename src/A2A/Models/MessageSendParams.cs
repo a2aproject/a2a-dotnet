cad303f856ff154099d50075a0b0a43dcf3e22fe
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2025-08-25 10:27:15 -0700

    Fix for polymorphic serialization to occur
    An internal property that is serialized/deserialized, but as the base
    type to trigger the correct polymorphic serialization behavior,
    specifically the addition of the discriminator value.
    We Obsolete and hide this, however, so that it's not inadvertently
    used by developers since it's intended ONLY for serialization purposes

--- src/A2A/Models/MessageSendParams.cs
+++ src/A2A/Models/MessageSendParams.cs
@@ -12,11 +12,21 @@ namespace A2A;
 public sealed class MessageSendParams
 {
     /// <summary>
+    /// The base-typed value of the message being sent to the server, required for JSON serialization to properly handle serializing the descriminator value.
+    /// </summary>
+    /// <remarks>We hide this from external devs as it's strictly a serialization nuance due to JSON polymorphism and discriminators.</remarks>
+    [JsonInclude, JsonPropertyName("message"), JsonRequired, Obsolete("This property is only to be used during internal de/serialization", error: false)]
+    internal A2AEvent JsonValue
+    {
+        get => this.Message;
+        set => this.Message = (Message)value;
+    }
+
+    /// <summary>
     /// The message being sent to the server.
     /// </summary>
-    [JsonPropertyName("message")]
-    [JsonRequired]
-    public Message Message { get; set; } = new Message();
+    [JsonIgnore]
+    public Message Message { get; set; } = new();
 
     /// <summary>
     /// Send message configuration.
